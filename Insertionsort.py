b71c4982341253a06dc276933b1fdb5594a639bb
akashkurra472 <86678780+akashkurra472@users.noreply.github.com>
2023-07-16 06:25:21 -0500

    Update Insertionsort.py

--- Insertionsort.py
+++ Insertionsort.py
@@ -2,13 +2,10 @@ def insertionsort(a):
     
     for i in range(1, len(a)): ##traversal starts from 1 to lenght of an input array
         pivot = a[i] ##second element in the array was stored in pivot variable 
-    
-    ##hence inorder to sort the array from first element store that in the variable j 
-    
+        
+        ##hence inorder to sort the array from first element store that in the variable j 
         j = i - 1
-    
-    ##comparision of the two seqential elements 
-    
+        ##comparision of the two seqential elements 
         while j >= 0 and pivot < a[j] :
             a[j+1] = a[j]
             j=j-1
@@ -22,3 +19,5 @@ a = [10,15,7,2,9]
 insertionsort(a)
 for i in range(len(a)): ## retriving each element from the sorted array and then printing one by one 
     print("%d" %a[i])
+
+----------------------------##ash--------------------------------------------------------------------
